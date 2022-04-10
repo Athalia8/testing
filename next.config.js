31cd0015aa158d96a246a753f9afb7fb09e7f1ea
ciptowi <ciptowidarto@gmail.com>
2022-04-10 13:21:26 +0700

    profile page & signin method

--- next.config.js
+++ next.config.js
@@ -9,6 +9,15 @@ module.exports = {
     messagingSenderId: "984262885855",
     appId: "1:984262885855:web:2444844f011e227e05446a",
   },
+  // reactStrictMode: true,
+  images: {
+    domains: ["i.ibb.co"],
+  },
+
+  // images: {
+  //   loader: 'imgix',
+  //   path: 'https://i.ibb.co/H4f3Hkv/profile.png',
+  // },
   async rewrites() {
     return [
       {
